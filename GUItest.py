9679c5e39631f921ec7e5e43f8c77ce2974ac027
Manuel Ulreich <Manuel@Mac-Manuel.emotions.at>
2020-03-26 22:16:12 +0100

    Fixes after testing on macos

--- GUItest.py
+++ GUItest.py
@@ -1,29 +1,32 @@
-from tkinter import *
-from tkinter.ttk import *
+# from tkinter import *
+# from tkinter.ttk import *
+import tkinter as tk
+from tkinter import ttk
 
 def main():
-    window = Tk()
+    window = tk.Tk()
     window.title("Hello world!")
     window.geometry('1920x1080')
     
-    lbl = Label(window, text="LabelText", font=("Arial Bold", 24))
+    lbl = tk.Label(window, text="LabelText", font=("Arial Bold", 24))
     lbl.grid(column=0, row=0)
     
-    label2 = Label(window, text="", font=("Arial Bold", 24))
+    label2 = tk.Label(window, text="", font=("Arial Bold", 24))
     label2.grid(column=0,row=1)
 
-    txt = Entry(window, width=10)
+    txt = tk.Entry(window, width=10)
     txt.grid(column=1,row=1)
     
     def clicked():
         lbl.configure(text="You clicked me!?!?")
         label2.configure(text=txt.get())
-    btn = Button(window, text="I am a button", bg="orange", fg="blue", command=clicked)
+    btn = ttk.Button(window, text="I am a button", command=clicked)#, highlightbackground='#3E4149')
+    # btn2 = ttk.Button(window, text="Button2", highlightbackground="red")
     btn.grid(column=1,row=0)
     
     txt.focus()
 
-    combo = Combobox(window)
+    combo = ttk.Combobox(window)
     combo.grid(row=2,column=1)
 
     window.mainloop()

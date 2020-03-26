42eb1d45a34faefb2af380f3efc564c5db6ac6dc
Manuel Ulreich <manuel@emotions.at>
2020-03-26 17:37:54 +0100

    Extended HelloWorld + added path to virtual python environment in workspace.

--- GUItest.py
+++ GUItest.py
@@ -1,7 +1,28 @@
 from tkinter import *
 
-window = Tk()
 
-window.title("Hello world!")
+def main():
+    window = Tk()
+    window.title("Hello world!")
+    window.geometry('1920x1080')
+    
+    lbl = Label(window, text="LabelText", font=("Arial Bold", 24))
+    lbl.grid(column=0, row=0)
+    
+    label2 = Label(window, text="", font=("Arial Bold", 24))
+    label2.grid(column=0,row=1)
 
-window.mainloop()
+    txt = Entry(window, width=10)
+    txt.grid(column=1,row=1)
+    
+    def clicked():
+        lbl.configure(text="You clicked me!?!?")
+        label2.configure(text=txt.get())
+    btn = Button(window, text="I am a button", bg="orange", fg="blue", command=clicked)
+    btn.grid(column=1,row=0)
+    
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
+    

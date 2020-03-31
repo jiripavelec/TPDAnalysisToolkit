a99f23ceb8b0d09f2eaff94ee654654a9a132b15
Manuel Ulreich <manuel@emotions.at>
2020-03-31 16:06:00 +0200

    First correct layout succesful

--- ToolkitGUI.py
+++ ToolkitGUI.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 class MainFrame(tk.Frame):
-    def __init__(self, parent, controller):
+    def __init__(self):#, parent, controller):
         super().__init__()
         self.initUI()
 
@@ -10,9 +10,26 @@ class MainFrame(tk.Frame):
         self.master.title("TPD Toolkit")
         self.pack(fill=tk.BOTH, expand=True)
 
-        leftFrame = tk.Frame(self)
-        
+        leftFrame = tk.Frame(self,bg='blue')
+        leftFrame.pack(side = tk.LEFT, fill=tk.BOTH)
+
+        leftlbl = tk.Label(leftFrame, text="Left Frame")
+        leftlbl.pack(side=tk.LEFT, padx=5, pady=5)
 
 
-        rightFrame = tk.Frame(self)
+        rightFrame = tk.Frame(self,bg='red')
+        rightFrame.pack(side = tk.RIGHT, fill=tk.BOTH, expand=True)
+
+        rightlbl = tk.Label(rightFrame, text="Right Frame")
+        rightlbl.pack(side=tk.LEFT, padx=5, pady=5)
+
+
+def main():
+    root = tk.Tk()
+    root.geometry("1920x1080")
+    main = MainFrame()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
 

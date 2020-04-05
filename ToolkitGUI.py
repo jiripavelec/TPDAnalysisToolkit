dc362365ae817d57811ece54a97643ab246ca230
Manuel Ulreich <manuel@emotions.at>
2020-04-05 22:04:46 +0200

    Added basic controls for file selection via listbox

--- ToolkitGUI.py
+++ ToolkitGUI.py
@@ -3,6 +3,9 @@ import tkinter.ttk as ttk
 from datetime import datetime
 from ControlsFrame import * #ui element
 from PlotsFrame import PlotsFrame
+from tkinter.filedialog import askdirectory
+from os import listdir
+from os.path import isfile, join
 
 # from dt import timedelta
 
@@ -12,57 +15,52 @@ class ProcessingStepControls:
         self.m_chordInitDone = False
         self.m_notebookInitDone = False
         self.mplContainers = []
+        self.m_filesDirectory = ""
 
     def initChordUI(self, parent):
         raise NotImplementedError()
 
-
-    # def getControlChord(self,parent, plotsFrame):
-    #     if(not self.m_chordInitDone):
-    #         self.m_chord = Chord(parent, self.getNotebook(plotsFrame), title=self.m_title)
-    #         self.initChordUI()
-
-    #     return self.m_chord
-
     def initNotebook(self, parent):
         raise NotImplementedError()
-
-    # def getNotebook(self,parent):
-    #     if(not self.m_notebookInitDone):
-    #         self.m_notebook = ttk.Notebook(parent)
-    #         self.initNotebook()
-
-    #     return self.m_notebook
-
-    # def chordWasClicked(self, chord):
-    #     self.m_notebook.tkraise()
 
     def setResizeTime(self):
         for c in self.mplContainers:
             c.resizeDateTime = datetime.now()
 
+    def selectFolder(self):
+        raise NotImplementedError()
+
+
 class ProcessTPDData(ProcessingStepControls):
     def __init__(self):
         super().__init__("Process TPD Data")
 
+    def selectFolder(self):
+        self.m_filesDirectory = askdirectory()
+        [self.m_listBox.insert(0, f) for f in listdir(self.m_filesDirectory) if isfile(join(self.m_filesDirectory, f))]
 
     def initNotebook(self, parent):
         self.m_notebook = ttk.Notebook(parent)
 
-        self.mplContainers.append(MPLContainer(self.m_notebook, "First graph", bg="white"))
-        self.mplContainers.append(MPLContainer(self.m_notebook, "Second graph", bg="white"))
-        # self.tab1 = MPLContainer(self.m_notebook, bg="white")
-        # self.tab2 = ttk.Frame(self.m_notebook)
+        self.mplContainers.append(MPLContainer(self.m_notebook, "Raw Data", bg="white"))
+        self.mplContainers.append(MPLContainer(self.m_notebook, "Processed Data", bg="white"))
+
         for c in self.mplContainers:
             self.m_notebook.add(c, text = c.m_title)
-        # self.m_notebook.add(self.tab1,text="first")
-        # self.m_notebook.add(self.tab2,text="second")
-        # self.m_notebook.pack(expand=1,fill='both')
+
         self.m_notebook.grid(row=0,column=0,sticky="nsew")
 
     def initChordUI(self, parent):
         self.m_chord = Chord(parent, self.m_notebook, title=self.m_title)
-        tk.Label(self.m_chord, text='hello world', bg='white').pack()
+
+        self.m_label = tk.Label(self.m_chord, text='Input files:')
+        self.m_label.pack(side = tk.TOP, fill = tk.X, expand = False)
+
+        self.m_listBox = tk.Listbox(self.m_chord, selectmode = tk.EXTENDED)
+        self.m_listBox.pack(side = tk.TOP, fill = tk.X, expand = False, padx = 5, pady = 5)
+
+        self.m_folderButton = ttk.Button(self.m_chord,text="Select folder",command = self.selectFolder)
+        self.m_folderButton.pack(side = tk.TOP, fill = tk.X, expand = False)
 
 class InvertTPDData(ProcessingStepControls):
     def __init__(self):

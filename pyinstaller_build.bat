97485d7a897e2491e7fa08344f5ffd9b4c724739
Manuel Ulreich <manuel@emotions.at>
2020-06-18 15:49:15 +0100

    gitignore and build.bat update

--- pyinstaller_build.bat
+++ pyinstaller_build.bat
@@ -1,1 +1,1 @@
-pyinstaller --paths "C:\GitRepos\TPD Toolkit\TPDAnalysisToolkit\venv\Lib\site-packages" ToolkitGUI.py --hidden-import pkg_resources.py2_warn
+pyinstaller --paths "C:\Repos\TPD\venv\Lib\site-packages" ToolkitGUI.py --hidden-import pkg_resources.py2_warn

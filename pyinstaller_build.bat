a77f3eb02271fcbba937ca3cce34371606e5945c
Manuel Ulreich <manuel@emotions.at>
2020-06-22 14:04:22 +0100

    Change to hidden imports for pyinstaller

--- pyinstaller_build.bat
+++ pyinstaller_build.bat
@@ -1,1 +1,1 @@
-pyinstaller --paths "C:\Repos\TPD\venv\Lib\site-packages" ToolkitGUI.py --hidden-import [ pkg_resources.py2_warn, pyexpat] --noconfirm
+pyinstaller --paths "C:\Repos\TPD\venv\Lib\site-packages" ToolkitGUI.py --hidden-import pkg_resources.py2_warn --hidden-import pyexpat --noconfirm

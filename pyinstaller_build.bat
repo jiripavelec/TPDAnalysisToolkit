cc0cb462fd840d07dff73399c60398f8f7fb3a4c
Manuel Ulreich <manuel@emotions.at>
2020-07-10 13:11:42 +0100

    updated hidden impors

--- pyinstaller_build.bat
+++ pyinstaller_build.bat
@@ -1,1 +1,1 @@
-pyinstaller --paths "C:\Repos\TPD\venv\Lib\site-packages" ToolkitGUI.py --hidden-import pkg_resources.py2_warn --hidden-import pyexpat --noconfirm
+pyinstaller --paths "C:\Repos\TPD\venv\Lib\site-packages" ToolkitGUI.py --hidden-import pkg_resources.py2_warn --hidden-import pyexpat --hidden-import pyi_rth_pkgres.py --noconfirm

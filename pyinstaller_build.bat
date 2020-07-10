62086fafe70a85a89e34c13f95508aaa59f69c6d
Manuel Ulreich <manuel@emotions.at>
2020-07-10 13:22:51 +0100

    Removed build and dist

--- pyinstaller_build.bat
+++ pyinstaller_build.bat
@@ -1,1 +1,1 @@
-pyinstaller --paths "C:\Repos\TPD\venv\Lib\site-packages" ToolkitGUI.py --hidden-import pkg_resources.py2_warn --hidden-import pyexpat --hidden-import pyi_rth_pkgres.py --noconfirm
+pyinstaller --paths "C:\Repos\TPD\venv\Lib\site-packages" ToolkitGUI.py --hidden-import pkg_resources.py2_warn --onedir --noconfirm

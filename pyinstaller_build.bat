a2f31e438f7aa8d437d98f972ff32f6b82d46cac
Manuel Ulreich <manuel@emotions.at>
2020-07-28 10:38:03 +0100

    Another attempt at standalone version.

--- pyinstaller_build.bat
+++ pyinstaller_build.bat
@@ -1,1 +1,1 @@
-pyinstaller --paths "C:\Repos\TPD\venv\Lib\site-packages" ToolkitGUI.py --hidden-import distutils --hidden-import pkg_resources --hidden-import pkg_resources.py2_warn --hidden-import pyimod03_importers.py --onedir --noconfirm ToolkitGUI.spec 
+pyinstaller --paths "C:\Repos\TPD\venv\Lib\site-packages" ToolkitGUI.py ToolkitGUI.spec 

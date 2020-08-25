49cb1a9056e301785e317232cafa33d4adb252be
Manuel Ulreich <manuelulreich@e250-242.eduroam.tuwien.ac.at>
2020-08-25 15:10:50 +0200

    Working on PeakIntegrationControl

--- DataControls/PeakIntegrationControl.py
+++ DataControls/PeakIntegrationControl.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from PlotsFrame import MPLContainer # pylint: disable=import-error
 from DataControls.ControlElements import Chord, ScrolledListBox, EnhancedCheckButton, ProcessingStepControlBase, EnhancedEntry, DisplayOptionsFrame, SingleInputFileSelectionControl #ui element # pylint: disable=import-error
+from DataModels.ProcessedDataWrapper import ProcessedDataWrapper # pylint: disable=import-error
 from tkinter.filedialog import askopenfilename
 
 class PeakIntegrationControl(ProcessingStepControlBase):
@@ -9,6 +10,16 @@ class PeakIntegrationControl(ProcessingStepControlBase):
         super().__init__("Peak Integration (WIP)", controller, accordion)
         self.m_parsedData = None
         self.m_plots["Processed Data"] = MPLContainer(self.m_chord.m_notebookRef, "Processed Data", "Desorption Rate (arb. U.)", "Temperature (K)", root)
+
+    def onFileSelected(self):
+        self.m_parsedData = ProcessedDataWrapper(self.m_fileSelectionControl.m_inputFilePath)
+        self.m_spectrumSelectionLabel = self.m_parsedData.
+
+    def onSpectrumSelected(self):
+        pass
+
+    def onBoundsChanged(self):
+        pass
 
     def initChordUI(self):
         self.m_chordFrame = self.m_chord.m_scrollable_frame
@@ -18,20 +29,43 @@ class PeakIntegrationControl(ProcessingStepControlBase):
         self.m_fileSelectionControl = SingleInputFileSelectionControl(self.m_chordFrame)
         self.m_fileSelectionControl.grid(row=0, column = 0, columnspan = 4, sticky = "nsew")
 
+        # Spectrum selection
+
+        self.m_spectrumSelectionLabel = ttk.Label(self.m_chordFrame, text='Select TPD Spectrum for Integration:')
+        self.m_spectrumSelectionLabel.grid(row = 1, column = 0, columnspan = 2, sticky="nsw")
+
+        self.m_spectrumCB = ttk.Combobox(self.m_chordFrame)
+        # self.m_prefactorCB.bind("<<ComboboxSelected>>", self.plotDataForSelectedPrefactor) #binding to event because CB does not have 'command' param
+        self.m_spectrumCB.grid(row = 2, column = 0, columnspan = 3, sticky = "nsew")
+        
+        # self.m_spectrumSelectionLabel = ttk.Label(self.m_chordFrame, text='Select Mass Spectrum for Integration:')
+        # self.m_spectrumSelectionLabel.grid(row = 3, column = 0, columnspan = 2, sticky="nsw")
+
+        # self.m_spectrumCB = ttk.Combobox(self.m_chordFrame)
+        # # self.m_prefactorCB.bind("<<ComboboxSelected>>", self.plotDataForSelectedPrefactor) #binding to event because CB does not have 'command' param
+        # self.m_spectrumCB.grid(row = 4, column = 0, columnspan = 3, sticky = "nsew")
+
         # Integration Options
 
         self.m_optionsLabel = ttk.Label(self.m_chordFrame, text="Integration Options:")#, compound = tk.CENTER)
-        self.m_optionsLabel.grid(row=1, column = 0, sticky = "nsw")
+        self.m_optionsLabel.grid(row=5, column = 0, sticky = "nsw")
         
         self.m_tCutStartLabel = ttk.Label(self.m_chordFrame, text="Initial Temperature:")
-        self.m_tCutStartLabel.grid(row=4, column = 1, sticky = "nse")
+        self.m_tCutStartLabel.grid(row=6, column = 1, sticky = "nse")
 
         self.m_tCutStartEntry = EnhancedEntry(self.m_chordFrame)
-        self.m_tCutStartEntry.grid(row=4, column = 2, sticky = "nsw")
+        self.m_tCutStartEntry.grid(row=6, column = 2, sticky = "nsw")
 
         self.m_tCutEndLabel = ttk.Label(self.m_chordFrame, text="Final Temperature:")
-        self.m_tCutEndLabel.grid(row=5, column = 1, sticky = "nse")
+        self.m_tCutEndLabel.grid(row=7, column = 1, sticky = "nse")
 
         self.m_tCutEndEntry = EnhancedEntry(self.m_chordFrame)
-        self.m_tCutEndEntry.grid(row=5, column = 2, sticky = "nsw")
+        self.m_tCutEndEntry.grid(row=7, column = 2, sticky = "nsw")
+
+        # Display Options
+
+        self.m_showBoundsCB = EnhancedCheckButton(self.m_chordFrame, text="Show Temp. Bounds")
+
+
+
 
